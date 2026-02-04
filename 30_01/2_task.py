e78c051e8282b17c204fbceb4a029cfef0f2821b
Nikita <sso0kk207@gmail.com>
2026-02-04 09:59:12 +0300

    Done 1 and 2 tasks

--- 30_01/2_task.py
+++ 30_01/2_task.py
@@ -1,35 +1,39 @@
 from collections import Counter
+import re
+
+summary_response = errors = gets = posts = puts = deletes = 0
+
+pattern = r'(?P<ip>\S+) - - \[(?P<date>.*?)\] "(?P<method>\w+) (?P<path>\S+) (?P<proto>.*?)" (?P<status>\d+) (?P<size>\d+) "(?P<agent>.*?)" (?P<time>\d+\.\d+)'
+
+ips = []
+agents = []
+
 try:
     with open("30_01/access.log", "r") as f:
-        data = f.readlines()
-
-    gets = data.count("GET")
-    posts = data.count("POST")
-    puts = data.count("PUT")
-    deletes = data.count("DELETE")
-
-    logs = []
-    summary_response = 0
-    errors = 0
-    ips = []
-    agents = []
-
-    for st in data:
-        st = st.replace(" - - ", " ").replace('"', "").replace('\n', "")
-        logs.append(st.split(" "))
-
-    for log in logs:
-        summary_response += float(log[-1])
-        if log[5][0] in ['4', '5']:
-            errors += 1
-        ips.append(log[0])
-        agents.append(log[-2].split('/', 1)[0])
-
-    avg = summary_response / len(logs)
-
-    print(f"====== Результаты анализа логов ======\nСреднее время ответа: {avg:.2f}\nКоличество ошибок: {errors}\nСамый популярный User-Agent: {Counter(agents).most_common(1)}\nТоп 10 по популярности ip:")
-    print(Counter(ips).most_common(10))
-
+        for st in f:
+            match = re.match(pattern, st)
+            if not match:
+                continue
+            res = match.groupdict()
+            summary_response += float(res['time'])
+            if res['status'].startswith(("4", "5")):
+                errors += 1
+            if res['method'] == "GET":
+                gets += 1
+            elif res['method'] == "POST":
+                posts += 1
+            elif res['method'] == "PUT":
+                puts += 1
+            elif res['method'] == "DELETE":
+                deletes += 1
+            ips.append(res['ip'])
+            agents.append(res['agent'].split('/')[0])
 
 except FileNotFoundError:
     print('Файл не был найден')
+
+avg = summary_response / len(ips) if ips else 0
+
+print(f"====== Результаты анализа логов ======\nКоличество методов GET: {gets}\nКоличество методов POST: {posts}\nКоличество методов PUT: {puts}\nКоличество методов DELETE: {deletes}\nСреднее время ответа: {avg:.2f}\nКоличество ошибок: {errors}\nСамый популярный User-Agent: {Counter(agents).most_common(1)[0][0]}\nТоп 10 по популярности ip:")
+for ip in enumerate(dict(Counter(ips).most_common(10)).keys(), start=1):
+    print(ip)
